8b4e8f9570caa394a2e1ddce7e641c7eed128c4a
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-24 20:21:17 +0100

    extensions including toggle priority

--- src/app.js
+++ src/app.js
@@ -4,13 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-        todos: ["Buy shopping", "Clean bathroom", "Car's MOT"],
-        newToDoItem:""
+        todos: [
+          {item: "Buy shopping", priority:"High"},
+          {item: "Clean bathroom", priority:"Low"},
+          {item: "Car's MOT", priority:"High"}
+        ],
+        newToDoitem:"",
+        newPriority:""
     },
     methods: {
         saveToDoItem: function () {
-            this.todos.push(this.newToDoItem);
-            this.newToDoItem = "";
+            this.todos.push({
+              item: this.newToDoItem,
+              priority: this.newPriority
+            });
+            this.newToDoItem = "",
+            this.newPriority = "";
+        },
+        togglePriority: function(index, priority) {
+          if (this.todos[index].priority==='High'){
+            this.todos[index].priority='Low';
+          } else {
+            this.todos[index].priority='High';
+          }
         }
     }
   });
